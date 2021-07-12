2a75e2b9083b05ec1dc66b828c062daf678b81a7
Phil Wolstenholme <p.wolstenholme@ctidigital.com>
2021-07-12 12:02:27 +0100

    DERP-346 - Add Babel for JS processing and npm script for cleaning dist directory before build

---
+++ babel.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  presets: ['@babel/preset-env'],
+  plugins: [
+    [
+      // Add a banner to help prevent devs editing dist files.
+      '@comandeer/babel-plugin-banner',
+      {
+        banner: `/*
+ * Do not edit files in the 'dist' directory, edit the source version in the 
+ * 'components' directory instead.
+ */`,
+      },
+    ],
+  ],
+  env: {
+    production: {
+      presets: [
+        [
+          'minify',
+          {
+            builtIns: false,
+          },
+        ],
+      ],
+      // Remove the banner to save some file weight.
+      plugins: [['@comandeer/babel-plugin-banner', false]],
+    },
+  },
+  ignore: [
+    (filename) => filename.match(/\.story\.js?$/)
+  ]
+};
