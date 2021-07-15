2880c5296befcdd05f4b2697d667a164d8c5e634
Phil Wolstenholme <p.wolstenholme@ctidigital.com>
2021-07-15 13:16:11 +0100

    DERP-346 - Remove PHP script to create copy of theme and use a Bash script instead (for now)

--- scripts/create-subtheme.sh
+++ scripts/create-subtheme.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# Script to quickly create sub-theme.
+
+# This is a basic approach based on https://git.drupalcode.org/project/bootstrap_sass/-/blob/8.x-1.x/scripts/create_subtheme.sh
+# In the future, we could borrow the approch that Drupal 10 will take and use a Symfony command to make the copy
+# of the theme: https://git.drupalcode.org/project/drupal/-/blob/9.3.x/core/lib/Drupal/Core/Command/GenerateTheme.php
 
 # Don't forget to set the right permissions. From the theme directory run:
 # chmod 777 scripts/create-subtheme.sh
@@ -28,8 +31,11 @@ for file in config/*/*gla_core_theme.*; do mv $file ${file//gla_core_theme/$NEW_
 rm scripts/create-subtheme.sh
 
 grep -Rl gla_core_theme .|xargs sed -i -e "s/gla_core_theme/$NEW_THEME_MACHINE_NAME/"
-sed -i -e "s/GLA accelerator theme/$NEW_THEME_HUMAN_NAME/" $NEW_THEME_MACHINE_NAME.info.yml
-sed -i -e "s|gla/accelerator-theme|gla/$NEW_THEME_MACHINE_NAME|" composer.json
+sed -i -e "s/GLA core theme/$NEW_THEME_HUMAN_NAME/" $NEW_THEME_MACHINE_NAME.info.yml
+sed -i -e "s/gla_core_theme/$NEW_THEME_MACHINE_NAME/" $NEW_THEME_MACHINE_NAME.info.yml
+sed -i -e "s/gla_core_theme/$NEW_THEME_MACHINE_NAME/" $NEW_THEME_MACHINE_NAME.libraries.yml
+sed -i -e "s/gla_core_theme/$NEW_THEME_MACHINE_NAME/" $NEW_THEME_MACHINE_NAME.breakpoints.yml
+sed -i -e "s|gla/gla_core_theme|gla/$NEW_THEME_MACHINE_NAME|" composer.json
 
 # Remove the backups that sed made. This is easier than finding a cross-platform version of the
 # sed command that doesn't produce backups.
