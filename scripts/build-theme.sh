9b9a2d38d8370791b3511e161128504e3d711cfb
Phil Wolstenholme <p.wolstenholme@ctidigital.com>
2021-07-16 10:32:50 +0100

    DERP-333 - Update theme build script to remove nvm usage

--- scripts/build-theme.sh
+++ scripts/build-theme.sh
@@ -1,10 +1,11 @@
-# Set the correct Node and npm version:
-. ~/.nvm/nvm.sh
-nvm use
+# Normally we'd run `nvm use` here to read from the theme's nvm file but the
+# web servers where the theme is built should already have a suitable version
+# of Node, managed by the GLA.
 
 # We use Composer to bring in third-party libraries. This step may have already
 # been run by the main `composer install` command for the project but it is
-# included in this script for testing
+# included in this script for testing theme builds outside of a project, e.g.
+# when developing inside the theme repository rather than a project repository.
 composer install
 
 # Install Node dependencies so we can build the theme
@@ -15,10 +16,10 @@ npm install
 # Storybook: `npm run build-prod --skip-storybook`
 npm run build-prod
 
-# On non-local environments only:
 # Once the theme has built itself (compiled JS, CSS, built Storybook etc)
 # then there's no need to actually deploy the vendor or node_modules
 # directories, and we can delete them. This way, we cut down on the amount
-# of third-party code that is deployed onto the web servers.
+# of third-party code that is deployed onto the web servers which may have
+# a security benefit.
 rm -rf node_modules
 rm -rf vendor
