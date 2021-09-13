f0172c87ad9301f42819cb14df414449816ffaa5
Phil Wolstenholme <p.wolstenholme@ctidigital.com>
2021-09-13 11:22:45 +0100

    DERP-227 - We don't want to use jQuery - we can use Cash instead

--- components/03-organisms/content-listing/content-listing.js
+++ components/03-organisms/content-listing/content-listing.js
@@ -1,16 +1,20 @@
-(function contentListing($, Drupal) {
+(function contentListing($$, Drupal) {
   Drupal.behaviors.glaContentListing = {
     attach(context) {
-      $('[data-content-listing]', context)
-        .once('gla-core-theme-content-listing')
-        .each(function eachContentListing() {
-          const $pagerLinks = $(this).find('[data-pager] li a');
-          const id = $(this).attr('id');
+      $$(
+        once('gla-core-theme-content-listing', '[data-content-listing]'),
+        context,
+      ).each(function eachContentListing() {
+        // Add an anchor to each link to act as a jump link and scroll the
+        // page back down to the content listing component. This relies on
+        // the component having the correct data attributes and an ID.
+        const $pagerLinks = $$(this).find('[data-pager] li a');
+        const id = $$(this).attr('id');
 
-          $pagerLinks.each(function rewriteHrefs() {
-            $(this).attr('href', `${$(this).attr('href')}#${id}`);
-          });
+        $pagerLinks.each(function rewriteHrefs() {
+          $$(this).attr('href', `${$$(this).attr('href')}#${id}`);
         });
+      });
     },
   };
-})(jQuery, Drupal);
+})(Cash, Drupal);
